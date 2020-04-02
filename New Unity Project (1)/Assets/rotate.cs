1627c59db1cda1ebe190a5c8a77fbd8e8274ba1f
Aimee <10893245@my.uvu.edu>
2020-04-01 21:32:05 -0600

    New game. Gitkraken hasnt been working, it wot push for days

---
+++ New Unity Project (1)/Assets/rotate.cs
@@ -0,0 +1,15 @@
+﻿
+using UnityEngine;
+
+public class rotate : MonoBehaviour
+{
+   public float spinx;
+   public float spinz;
+   public float spiny;
+   public float speed;
+
+   private void Start()
+   {
+      transform.Rotate(spinx,spiny,spinz);
+   }
+}
