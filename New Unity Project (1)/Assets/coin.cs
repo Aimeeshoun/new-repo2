5e9a13f1ff2a975ad75271083948515ea27b6149
Aimee <10893245@my.uvu.edu>
2020-04-03 21:02:36 -0600

    aaCoin Script

---
+++ New Unity Project (1)/Assets/coin.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEngine;
+
+public class coin : MonoBehaviour
+{
+   {
+         void OnTriggerEnter(Collider other)
+
+    {
+
+        if (other.CompareTag("Player"))
+
+        {
+
+
+            Destroy(other.gameObject);
+
+
+        }
+
+    }
+}
+}
